d8aae5602884f5cba5de39b46c554d6614234241
Lightsake-Sensei <805935229@qq.com>
2022-10-31 10:01:13 +0800

    fire getter

--- python/GetDataSaveToDbs.py
+++ python/GetDataSaveToDbs.py
@@ -50,7 +50,11 @@ def getData(*arg):
     ret = requests.post(url,data)
     json_dict = json.loads(ret.text)
     #print(json_dict)
-    buffer_time = ""
+    temp_time = ""
+    humi_time = ""
+    light_time = ""
+    lamp_time = ""
+    fire_time = ""
     while(True):
         Session = sessionmaker(engin)
         session = Session()
@@ -58,18 +62,54 @@ def getData(*arg):
         humi = getSensor(arg[2],'humidity', json_dict['ResultObj']['AccessToken'])
         lamp = getSensor(arg[2],'lamp', json_dict['ResultObj']['AccessToken'])
         light = getSensor(arg[2],'light', json_dict['ResultObj']['AccessToken'])
-        if buffer_time != temp["time"]:
+        fire = getSensor(arg[2],'fire', json_dict['ResultObj']['AccessToken'])
+        if temp_time != temp["time"]:
+            print("save...temp")
             session.add_all(
                 [
                     Sensor_data(created_at=temp['time'],updated_at=temp['time'],sensor_type='temperature',value=temp['value']),
+                ]
+            )
+            session.commit()
+            temp_time = temp["time"]
+        if humi_time != humi["time"]:
+            print("save...humi")
+            session.add_all(
+                [
                     Sensor_data(created_at=humi['time'],updated_at=humi['time'],sensor_type='humidity',value=humi['value']),
-                    Sensor_data(created_at=lamp['time'],updated_at=lamp['time'],sensor_type='lamp',value=lamp['value']),
+                ]
+            )
+            session.commit()
+            humi_time = humi["time"]
+        if light_time != light["time"]:
+            print("save...light")
+            session.add_all(
+                [
+            
                     Sensor_data(created_at=light['time'],updated_at=light['time'],sensor_type='light',value=light['value'])
                 ]
             )
             session.commit()
-            buffer_time = temp["time"]
-        time.sleep(10)
+            light_time = light["time"]
+        if lamp_time != lamp["time"]:
+            print("save...lamp")
+            session.add_all(
+                [
+                    Sensor_data(created_at=lamp['time'],updated_at=lamp['time'],sensor_type='lamp',value=lamp['value']),
+                ]
+            )
+            session.commit()
+            lamp_time = lamp["time"]
+        if fire_time !=fire["time"]:
+            print("save...fire")
+            session.add_all(
+                [
+                    Sensor_data(created_at=fire['time'],updated_at=fire['time'],sensor_type='fire',value=fire['value']),
+                ]
+            )
+            session.commit()
+            fire_time = fire["time"]
+        time.sleep(5)
 
 
 def getSensor(deviceId:str,tag:str,token:str)->dict:
